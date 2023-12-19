aaa05e227810e7064d4ba115d79528ca70faf172
NDAYISHIMIYE Emile <92234223+ndayiemile@users.noreply.github.com>
2023-12-19 12:45:26 +0200

    Add files via upload

---
+++ Library Management System2/libs/js/index.js
@@ -0,0 +1,20 @@
+// // variables
+// let sideNavbarToggler = document.getElementById("sideNavbar-toggler")
+// let sidebarTextNodes = document.querySelectorAll(".sidebar-text")
+// let Elementsdisplay = true
+// let rootCSS = document.querySelector(':root')
+
+// console.log(rootCSS)
+// sideNavbarToggler.onclick = () => {
+//     Elementsdisplay = !Elementsdisplay
+//         //side bar text hider
+//     sidebarTextNodes.forEach(element => {
+//         if (!Elementsdisplay) {
+//             element.classList.add("d-none")
+//         } else {
+//             element.classList.remove("d-none")
+//         }
+//         let Elementclass = element.classList
+//         console.log(Elementclass)
+//     });
+// }
